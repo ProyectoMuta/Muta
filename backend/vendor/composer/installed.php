8d88a6f719d80d145baf4043f766911a22aeca84
ElizabethMorato <elizabethmorato19@gmail.com>
2025-10-15 13:28:03 -0300

    actualizando

--- backend/vendor/composer/installed.php
+++ backend/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f21f712b2418c88b0a73d3ba15ae9461341a5496',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f21f712b2418c88b0a73d3ba15ae9461341a5496',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
